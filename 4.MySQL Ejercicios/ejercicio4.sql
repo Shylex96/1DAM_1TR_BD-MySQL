2ee875017785682c4f275155bda87286e7a469c7
Shylex96 <77785532+Shylex96@users.noreply.github.com>
2023-01-09 11:38:15 +0100

    Update ejercicio4.sql

--- 4.MySQL Ejercicios/ejercicio4.sql
+++ 4.MySQL Ejercicios/ejercicio4.sql
@@ -23,3 +23,55 @@ CREATE TABLE temas (
     FOREIGN KEY (idAsignaturaFK)
         REFERENCES asignaturas (idAsignatura)
 );
+
+-- Modificar la tabla 'Asignaturas'.
+-- Añadir al final el campo 'cursoAsignatura int'
+ALTER TABLE asignaturas ADD cursoAsignatura INT;
+
+-- Modificar la tabla 'Temas'.
+-- Añadir después de descripcionTema: nivelTema INT
+ALTER TABLE temas ADD nivelTema INT AFTER descripcionTema;
+
+-- Insertar datos en la tabla.
+-- Insertar 3 registros:
+INSERT INTO asignaturas VALUES(
+	null, 'Bases de Datos', 1
+);
+
+SELECT * FROM asignaturas;
+
+-- Programación -- 1
+-- Despliegue de Aplicaciones Web -- 2
+INSERT INTO asignaturas VALUES(
+	null, 'Programación', 1
+);
+
+INSERT INTO asignaturas VALUES(
+	null, 'Despliegue de Aplicaciones Web', 2
+);
+
+/* 
+Se puede ejecutar todo de esta forma para evitar hacer varios insert:
+
+INSERT INTO asignaturas VALUES(
+	null, 'Programación', 1
+),
+(
+	null, 'Despliegue de Aplicaciones Web', 2
+);
+
+*/
+
+SELECT * FROM asignaturas;
+
+-- Insertar datos en la tabla.
+INSERT INTO temas VALUES(
+	null, 1, 'Introducción a las Bases de Datos Relacionales',
+    1, 1
+);
+-- El último '1' de la línea 70 hace referencia al id donde se encuentra primary key de la tabla referida.
+
+SELECT * FROM temas;
+
+
+
